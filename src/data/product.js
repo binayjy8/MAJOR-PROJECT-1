2a4d30cd6e03552d448c2e9747887b439114009a
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-11-28 18:36:01 +0530

    added data

---
+++ src/data/product.js
@@ -0,0 +1,58 @@
+export const products = [
+  {
+    id: 1,
+    name: "Casual Cotton Shirt",
+    price: 799,
+    image: "/photo.jpg",
+    category: "Men",
+  },
+  {
+    id: 2,
+    name: "Stylish Sports Shoes",
+    price: 1599,
+    image: "/photo.jpg",
+    category: "Footwear",
+  },
+  {
+    id: 3,
+    name: "Classic Wrist Watch",
+    price: 1199,
+    image: "/photo.jpg",
+    category: "Accessories",
+  },
+  {
+    id: 4,
+    name: "Premium Denim Jeans",
+    price: 999,
+    image: "/photo.jpg",
+    category: "Men",
+  },
+  {
+    id: 5,
+    name: "Trendy Backpack",
+    price: 699,
+    image: "/photo.jpg",
+    category: "Bags",
+  },
+  {
+    id: 6,
+    name: "Cotton Oversized T-shirt",
+    price: 499,
+    image: "/photo.jpg",
+    category: "Men",
+  },
+  {
+  id: 7,
+  name: "Comfort Running Track Pants",
+  price: 899,
+  image: "/photo.jpg",
+  category: "Men",
+},
+{
+  id: 8,
+  name: "Slim Fit Casual Jacket",
+  price: 1299,
+  image: "/photo.jpg",
+  category: "Men",
+}
+];
