c4e2215ba768580d75645a145d652f06e59760b9
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-11-30 19:34:16 +0530

    import context

--- src/pages/Men.jsx
+++ src/pages/Men.jsx
@@ -5,7 +5,6 @@ export default function Men() {
   return (
     <div className="product-page">
 
-      
       <div className="filter">
         <h3>Filters</h3>
         <a className="clear-filter">Clear</a>
