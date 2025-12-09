4683a4edf6135699a6f423f6d232daa5835f4007
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-12-09 11:42:31 +0530

    page update

--- src/App.js
+++ src/App.js
@@ -5,15 +5,13 @@ import Men from "./pages/Men";
 import ProductDetail from "./pages/ProductDe";
 import ProductDetails from "./pages/ProductDetails";
 import { Routes, Route } from "react-router-dom";
-
 import { ProductProvider } from "./context/ProductContext";
 import { CartProvider } from "./context/CartContext";
 import Wishlist from "./pages/Wishlist";
 import CartPage from "./pages/CartPage";
-import useFetch from "./pages/useFetch";
+import { useEffect } from "react";
 
 function App() {
-  const { data, loading, error, fetchData } = useFetch("https://project-backend-eta-pink.vercel.app/")
   return (
     <ProductProvider>
       <CartProvider>
@@ -23,8 +21,8 @@ function App() {
             <Route path="/" element={<AllProducts />} />
             <Route path="/men" element={<Men />} />
             <Route path="/product" element={<ProductDetail />} />
-            <Route path="detail" element={<ProductDetails />}/>
-            <Route path="/wishlist" element={< Wishlist/>}/>
+            <Route path="/detail/:id" element={<ProductDetails />} />
+            <Route path="/wishlist" element={<Wishlist />} />
             <Route path="/cart" element={<CartPage />} />
           </Routes>
         </div>
