99f259555e86485740edc67438a668d2a69a35f6
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-12-29 21:06:13 +0530

    updated

--- src/pages/Kids.jsx
+++ src/pages/Kids.jsx
@@ -12,7 +12,7 @@ export default function Kids() {
 
   const safeProducts = Array.isArray(products) ? products : [];
 
-  // Filter fo
+  // Filter f
   const kidsProducts = safeProducts.filter(product => {
     const productCategory = typeof product.category === 'object' 
       ? product.category.name 
