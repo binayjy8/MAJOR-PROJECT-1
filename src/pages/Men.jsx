b281d68ab5c2607755b1298e6ddc6ec154115571
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-12-31 22:42:58 +0530

    page updated

--- src/pages/Men.jsx
+++ src/pages/Men.jsx
@@ -12,7 +12,7 @@ export default function Men() {
 
   const safeProducts = Array.isArray(products) ? products : [];
 
-  // Filter for Men's prod
+  // Filter for Men's pro
   const menProducts = safeProducts.filter(product => {
     const productCategory = typeof product.category === 'object' 
       ? product.category.name 
