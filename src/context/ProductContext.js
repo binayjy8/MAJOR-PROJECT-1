effcf6965f733752e39668c4aa7d89a9c0f5c81c
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2026-01-05 20:02:38 +0530

    page updated

--- src/context/ProductContext.js
+++ src/context/ProductContext.js
@@ -108,7 +108,7 @@ export function ProductProvider({ children }) {
     <ProductContext.Provider
       value={{
         products,
-        filteredProducts, // ✅ USE THIS EVERYW
+        filteredProducts, // ✅ USE THIS EVERY
         categories,
         loading,
         error,
