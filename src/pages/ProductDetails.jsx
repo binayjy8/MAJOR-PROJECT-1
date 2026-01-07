e998d319e083b84f76f809ce22f7e2cb1bad82c3
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2026-01-07 20:36:29 +0530

    page updated

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -117,7 +117,7 @@ export default function ProductDetails() {
           <p>✔ Secure Payment</p>
         </div>
 
-        {/* ✅ DESCRIPTION (MOVED BELOW FEATUR */}
+        {/* ✅ DESCRIPTION (MOVED BELOW FEATU */}
         <div className="description-section">
           <h3>Description</h3>
           <p>
