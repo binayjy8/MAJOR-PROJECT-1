983c1caf95fab629dae885f4d1ac132970cfeb03
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2026-01-08 12:26:53 +0530

    page updated

--- src/components/FrontPage.jsx
+++ src/components/FrontPage.jsx
@@ -47,7 +47,7 @@ export default function FrontPage() {
           <span>All Products</span>
         </div>
 
-        {/* OTH  */}
+        {/* OT  */}
         {categories.map((cat) => (
           <div
             key={cat._id}
