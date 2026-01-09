58eafcb8c4aba0b8634cd4026c051257aee70a56
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2026-01-09 08:40:55 +0530

    page updated

--- src/components/FrontPage.jsx
+++ src/components/FrontPage.jsx
@@ -40,7 +40,7 @@ export default function FrontPage() {
 
   return (
     <div className="home-page-container">
-      {/* CATEGORY S */}
+      {/* CATEGORY  */}
       <div className="category-strip">
         <div className="category-tile all-tile" onClick={handleAllProducts}>
           <span>All Products</span>
