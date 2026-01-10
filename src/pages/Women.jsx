9a80e7014a32432d80b75dbb41a1e402ef496e0f
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2026-01-10 21:11:31 +0530

    page updated

--- src/pages/Women.jsx
+++ src/pages/Women.jsx
@@ -94,7 +94,7 @@ export default function Women() {
           ))}
         </div>
 
-        {/* SO */}
+        {/* S */}
         <div className="filter-section">
           <p className="filter-title">Sort By</p>
           <label className="filter-option">
